763070dd2d43d45ca9c64969e6343a091390c47d
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-18 00:37:47 +0700

    Fix HelloWorld function return value and add skip condition for Windows in tests

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,21 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Tidak bisa berjalan di Windows")
+	}
+
+	result := HelloWorld("Ilham")
+	require.Equal(t, "Hello Ilham", result, "Hasil Harusnya 'Hello Ilham'")
+}
 
 func TestHelloWorldAssert(t *testing.T) {
 	result := HelloWorld("Ilham")
