7d777e08222660156212d296d6affb1874d378a4
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-17 23:10:22 +0700

    Refactor test cases in hello_world_test.go for clarity and consistency

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,35 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Ilham")
 
 	if result != "Hello Ilham" {
-		panic("Result salah / bukan Hello Ilham")
+		//if erro
+		// panic("Result salah / bukan Hello Ilham")
+
+		// t.Fail()
+		t.Error("Result harusnya 'hello ilham' ")
+		//t.Error untuk memberikan keterangan error
 	}
+
+	fmt.Println("Test HelloWorld Berhasil")
 }
 
 func TestHelloWorldLii(t *testing.T) {
 	result := HelloWorld("Lii")
 
 	if result != "Hello Lii" {
-		panic("Result salah / bukan Hello Ilham")
+		//if error
+		// panic("Result salah / bukan Hello Ilham")
+		// t.FailNow()
+		t.Fatal("Hasil harusnya 'Hello Lii'")
+		//t.Fatal memberikan keterangan error dan langsung keluar program
 	}
+
+	fmt.Println("Test HelloWorldLii Berhasil")
 }
